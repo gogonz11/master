d999a0ebf9be57fd20eb007a9356b11eecee94cd
Arif P <marifp@staff.undip.ac.id>
2022-10-03 10:20:49 +0700

    a

--- coba/materi/number.js
+++
@@ -1,5 +0,0 @@
-let angka1 = 0.1;
-let angka2 = 0.2;
-
-let hasil = ((angka1*10) * (angka2*10)/10);
-console.log(hasil);
--- coba/materi/string.js
+++
@@ -1,6 +0,0 @@
-let nama ="10";
-let alamat ='20';
-let NIK=`000000089 ${nama}`;
-
-let profil= nama + alamat;
-console.log(NIK);
